269c7cc088dd708689cd3d42d0be0125b3e5a1f2
Nurmuhammed <sagyndyk0006@gmail.com>
2025-08-13 16:53:18 +0500

    fix route problems

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import './index.css'
 import { Layout } from './Layout'
 
-
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Layout/>  
-   </BrowserRouter>
+  <HashRouter>
+    <Layout/>
+  </HashRouter>
 )
